aea8c67d2d07e00e1a3f76fb3342426216448cf6
zpershuai <zpershuai@gmail.com>
2023-05-31 15:43:07 +0800

    update nvim 0.9

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -4,7 +4,7 @@ if not status_ok then
 end
 
 configs.setup({
-    ensure_installed = { "cpp", "c", "lua" }, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+    ensure_installed = { "cpp", "c", "lua", "make" }, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
     sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
     ignore_install = { "" }, -- List of parsers to ignore installing
     autopairs = {
