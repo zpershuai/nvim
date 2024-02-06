2b31f990fd8c1abfcc43cf4ffdc0cbb4dfff4ed9
zpershuai <zpershuai@gmail.com>
2024-02-06 17:37:11 +0800

    add lazy.lua

---
+++ lua/user/lazy.lua
@@ -0,0 +1,34 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correct
+
+require("lazy").setup("user.plugins",{
+  defaults = { lazy = true },
+  install = { colorscheme = { "tokyonight" } },
+  checker = { enabled = true },
+  concurrency = 5,
+  performance = {
+    rtp = {
+      disabled_plugins = {
+        "gzip",
+        "netrwPlugin",
+        "tarPlugin",
+        "tohtml",
+        "tutor",
+        "zipPlugin",
+      },
+    },
+  },
+  debug = false,
+})
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -1,19 +1,4 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-
-vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correct
-
-require("lazy").setup({
+return {
   "folke/which-key.nvim",
   { "folke/neoconf.nvim", cmd = "Neoconf" },
   "folke/neodev.nvim",
@@ -108,4 +93,4 @@ require("lazy").setup({
   
   "romgrk/barbar.nvim",
   "dstein64/vim-startuptime",
-})
+}
